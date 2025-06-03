7f1daf8e92e57af7fa0e7037c121ae689141ce52
ion <ion@n0de.biz>
2025-06-03 17:55:39 +0000

    upd

--- modules/apps/desktop.nix
+++ modules/apps/desktop.nix
@@ -395,7 +395,145 @@
         };
 
       };
-    home = { };
+    home = { config, pkgs, ... }:
+      let
+        defaultApps = {
+          browser = [ "zen-beta.desktop" ];
+          text = [ "org.gnome.TextEditor.desktop" ];
+          image = [ "imv-dir.desktop" ];
+          audio = [ "mpv.desktop" ];
+          video = [ "mpv.desktop" ];
+          directory = [ "nemo.desktop" ];
+          office = [ "libreoffice.desktop" ];
+          pdf = [ "org.gnome.Evince.desktop" ];
+          terminal = [ "ghostty.desktop" ];
+          archive = [ "org.gnome.FileRoller.desktop" ];
+          discord = [ "webcord.desktop" ];
+        };
+
+        mimeMap = {
+          text = [ "text/plain" ];
+          image = [
+            "image/bmp"
+            "image/gif"
+            "image/jpeg"
+            "image/jpg"
+            "image/png"
+            "image/svg+xml"
+            "image/tiff"
+            "image/vnd.microsoft.icon"
+            "image/webp"
+          ];
+          audio = [
+            "audio/aac"
+            "audio/mpeg"
+            "audio/ogg"
+            "audio/opus"
+            "audio/wav"
+            "audio/webm"
+            "audio/x-matroska"
+          ];
+          video = [
+            "video/mp2t"
+            "video/mp4"
+            "video/mpeg"
+            "video/ogg"
+            "video/webm"
+            "video/x-flv"
+            "video/x-matroska"
+            "video/x-msvideo"
+          ];
+          directory = [ "inode/directory" ];
+          browser = [
+            "text/html"
+            "x-scheme-handler/about"
+            "x-scheme-handler/http"
+            "x-scheme-handler/https"
+            "x-scheme-handler/unknown"
+          ];
+          office = [
+            "application/vnd.oasis.opendocument.text"
+            "application/vnd.oasis.opendocument.spreadsheet"
+            "application/vnd.oasis.opendocument.presentation"
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+            "application/msword"
+            "application/vnd.ms-excel"
+            "application/vnd.ms-powerpoint"
+            "application/rtf"
+          ];
+          pdf = [ "application/pdf" ];
+          terminal = [ "terminal" ];
+          archive = [
+            "application/zip"
+            "application/rar"
+            "application/7z"
+            "application/*tar"
+          ];
+          discord = [ "x-scheme-handler/discord" ];
+        };
+
+        associations = with lib.lists;
+          listToAttrs (flatten (mapAttrsToList
+            (key: map (type: attrsets.nameValuePair type defaultApps."${key}"))
+            mimeMap));
+      in {
+        xdg = {
+          enable = true;
+          configFile."mimeapps.list".force = true;
+          mimeApps = {
+            enable = true;
+            associations.added = associations;
+            defaultApplications = associations;
+          };
+          userDirs = {
+            enable = true;
+            createDirectories = lib.mkDefault true;
+            extraConfig = {
+              XDG_SCREENSHOTS_DIR = "${config.home.homeDirectory}/Screenshots";
+            };
+          };
+        };
+
+        fonts.fontconfig = {
+          enable = true;
+          defaultFonts = {
+            serif = [ "Source Serif" "Noto Color Emoji" ];
+            sansSerif = [ "Work Sans" "Fira Sans" "Noto Color Emoji" ];
+            monospace = [
+              "FiraCode Nerd Font Mono"
+              "Font Awesome 6 Free"
+              "Font Awesome 6 Brands"
+              "Symbola"
+              "Noto Emoji"
+            ];
+            emoji = [ "Noto Color Emoji" ];
+          };
+        };
+        home.packages = with pkgs; [
+          ## Multimedia
+          audacity
+          gimp
+          obs-studio
+          pavucontrol
+          vlc
+          discord
+
+          ## Office
+          libreoffice
+          gnome-calculator
+          obsidian
+
+          ## Utility
+          dconf-editor
+          gnome-disk-utility
+          zenity
+          wl-clipboard # clipboard utils for wayland (wl-copy, wl-paste)
+          gopass-jsonapi
+        ];
+
+      };
   };
   defaultTags = { desktop = lib.mkDefault true; };
 }
