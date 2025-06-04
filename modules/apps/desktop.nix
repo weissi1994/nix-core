1ab197ee2278919c1c8159492c5ff4880e17c132
ion <daniel@n0de.biz>
2025-06-04 23:22:58 +0000

    upd

--- modules/apps/desktop.nix
+++ modules/apps/desktop.nix
@@ -558,6 +558,8 @@
         stylix = {
           enable = true;
           image = ./files/background.png;
+          base16Scheme =
+            "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
           polarity = "dark";
 
